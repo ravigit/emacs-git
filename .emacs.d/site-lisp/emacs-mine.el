86ba5105eeb5e45efe11f6dc07993a7b6bd8bcb8
Ravi Gorrepati <rgorrepati@apple.com>
2023-12-08 20:48:46 -0800

    - Emacs 29 support

--- .emacs.d/site-lisp/emacs-mine.el
+++ .emacs.d/site-lisp/emacs-mine.el
@@ -13,7 +13,7 @@
 (setq package-archives '(                        
                          ("melpa" . "http://melpa.org/packages/")
                          ("melpa-stable" . "http://stable.melpa.org/packages/")
-			 ("gnu" . "http://elpa.gnu.org/packages/")))
+			                ("gnu" . "http://elpa.gnu.org/packages/")))
 
 (add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
 
@@ -49,11 +49,12 @@
 
 (defvar local-pkgs
   (list 'emacs-os 'google 'grep-buffers 'hide-lines 'language-styles  'revbufs 'show-functions 'epigrams
-	;;'misc
-	'airline-themes
+	     ;;'misc
+	     'airline-themes
 	))
 
 (mapc 'require local-pkgs)
+(require 'lsp-mode)
 
 ;; (require 'airline-themes)
 
@@ -63,9 +64,9 @@
 (electric-pair-mode)
 (column-number-mode t)
 (desktop-save-mode 1)
- (fringe-mode)
+(fringe-mode)
 (global-hi-lock-mode 1)
-(global-linum-mode t)
+(global-nlinum-mode t)
 (scroll-bar-mode -1)
 (semantic-mode 1)
 (setq scroll-step 1)
@@ -85,7 +86,8 @@
 (setq whitespace-style '(trailing tabs newline tab-mark newline-mark))
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 3)
-
+(setq cider-repl-display-help-banner nil)
+(setq  cljr-suppress-middleware-warnings t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Add mode bindings to file types ;;
@@ -111,8 +113,7 @@
 (add-hook 'org-mode-hook (lambda () (flyspell-mode)))
 (add-hook 'clojure-mode-hook (lambda() (enable-paredit-mode)))
 (add-hook 'clojure-mode-hook (lambda() (rainbow-delimiters-mode)))
-(add-hook 'clojure-mode-hook (lambda()
-                               (yas-minor-mode 1)))
+(add-hook 'clojure-mode-hook (lambda() (yas-minor-mode 1)))
 (add-hook 'markdown-mode-hook (lambda() (flyspell-mode 1)))
 (add-hook 'markdown-mode-hook 'pandoc-mode)
 (add-hook 'pandoc-mode-hook 'pandoc-load-default-settings)
@@ -153,56 +154,56 @@
 ;; Key maps  ;;
 ;;;;;;;;;;;;;;;
 (let ((mappings (list
-                 '("\C-\\"     uncomment-line-or-region)
-                 '("\C-c'"     gnus)
-                 '("\C-c."     add-to-imports)
-                 '("\C-c/"     comment-line-or-region)
-                 '("\C-c0"     bm-show-all)
-                 '("\C-c1"     bm-toggle)
-                 '("\C-c2"     bm-next)
-                 '("\C-c3"     bm-previous)
-                 '("\C-c;"     search-index)
-                 '("\C-c?"     open-org)
-                 '("\C-c\C-b"  search-forward-regexp)
-                 '("\C-c\C-t"  restart-tomcat)
-                 '("\C-ca"     open-fileline)
-                 '("\C-cb"     grep-buffers)
-                 '("\C-cc"     compile)
-                 '("\C-cd"     vc-diff)
-                 '("\C-ce"     blank-mode)
-                 '("\C-cf"     grep)
-                 '("\C-cg"     google-it)
-                 '("\C-ch"     hide-lines)
-                 '("\C-ci"     transpose-frame)
-                 '("\C-cj"     join-line)
-                 '("\C-ck"     tags-apropos)
-                 '("\C-cl"     cider-connect) ; clojure uses cider
-                 '("\C-cn"     auto-revert-tail-mode)
-                 '("\C-co"     eval-buffer)
-                 '("\C-cp"     goto-line)
-                 '("\C-cq"     query-replace)
-                 '("\C-crf"    recursive-grep)
-                 '("\C-cs"     cscope-find-global-definition)
-                 '("\C-cu"     my-browse-url)
-                 '("\C-cv"     indent-buffer)
-                 '("\C-cy"     duplicate-line)
-                 '("\C-cx"     replace-string)
-                 '("\C-cz"     close-all-buffers)
-                 '("\M-["      beginning-of-defun)
-                 '("\M-]"      end-of-defun)
-                 '([f1]        search-forward-regexp)
-                 '([f2]        search-backward-regexp)
-                 '([f3]        xah-emacs-help)
-                 '([f5]        search-index)
+                 '("C-\\"		uncomment-line-or-region)
+					  '("C-c '"		gnus)
+					  '("C-c ."		add-to-imports)
+					  '("C-c /"		comment-line-or-region)
+					  '("C-c 0"		bm-show-all)
+					  '("C-c 1"		bm-toggle)
+					  '("C-c 2"		bm-next)
+					  '("C-c 3"		bm-previous)
+					  '("C-c ;"		search-index)
+					  '("C-c ?"		open-org)
+					  '("C-c C-b"	search-forward-regexp)
+					  '("C-c C-t"	restart-tomcat)
+					  '("C-c a"		open-fileline)
+					  '("C-c b"		grep-buffers)
+					  '("C-c c"		compile)
+					  '("C-c d"		vc-diff)
+					  '("C-c e"		blank-mode)
+					  '("C-c f"		grep)
+					  '("C-c g"		google-it)
+					  '("C-c h"		hide-lines)
+					  '("C-c i"		transpose-frame)
+					  '("C-c j"		join-line)
+					  '("C-c k"		tags-apropos)
+					  '("C-c l"		cider-connect) ; clojure uses cider
+					  '("C-c n"		auto-revert-tail-mode)
+					  '("C-c o"		eval-buffer)
+					  '("C-c p"		goto-line)
+					  '("C-c q"		query-replace)
+					  '("C-c r f"		recursive-grep)
+					  '("C-c s"		cscope-find-global-definition)
+					  '("C-c u"		my-browse-url)
+					  '("C-c v"		indent-buffer)
+					  '("C-c y"		duplicate-line)
+					  '("C-c x"		replace-string)
+					  '("C-c z"		close-all-buffers)
+					  '("M-["		beginning-of-defun)
+					  '("M-]"		end-of-defun)
+                 ;; Don't know how to get these working in emacs29
+                 ;; '([f1]        search-forward-regexp)
+                 ;; '([f2]        search-backward-regexp)
+                 ;; '([f3]        xah-emacs-help)
+                 ;; '([f5]        search-index)
                  )))
   (mapc (lambda (mapping)
           (let ((key (car mapping))
                 (func (cadr mapping)))
             (progn
               (message (format "map key %s to %s" key func))
-              (global-set-key key func))))
+              (keymap-global-set key func))))
         mappings))
-
 
 (defun disable-themes ()
   (interactive)
@@ -235,25 +236,43 @@
 	  (if this-win-2nd (other-window 1))))))
 
 
-(global-set-key (kbd "s-.") 'ripgrep-regexp)
-(global-set-key (kbd "s-a") 'package-list-packages)
-(global-set-key (kbd "s-c") 'cua-mode)
-(global-set-key (kbd "s-b") 'indent-buffer)
-(global-set-key (kbd "s-p") 'paredit-mode)
-(global-set-key (kbd "s-r") 'replace-regexp)
-(global-set-key (kbd "s-s") 'replace-string)
-(global-set-key (kbd "s-l") 'load-theme)
-(global-set-key (kbd "s-m") 'magit-status)
-(global-set-key (kbd "s-t") 'toggle-window-split)
-(global-set-key (kbd "s-v") 'magit-diff)
-(global-set-key (kbd "s-n") 'magit-blame)
-(global-set-key (kbd "s-/") 'comment-line-or-region)
-(global-set-key (kbd "s-\\") 'uncomment-line-or-region)
-(global-set-key (kbd "s-d") 'disable-themes)
-(global-set-key (kbd "s-w") 'whack-whitespace)
+(defun kill-all-cider-buffers ()
+  "Kill all cider buffers."
+  (interactive)
+  (cl-loop for buffer in (buffer-list)
+         when (string-match "cider-repl" (buffer-name buffer))
+         do
+         (kill-buffer buffer)))
 
 
-;; (define-key clojure-mode-map (kbd "M-r") 'cider-namespace-refresh)
+(keymap-global-set "s-." 'ripgrep-regexp)
+(keymap-global-set "s-a" 'package-list-packages)
+(keymap-global-set "s-c" 'cua-mode)
+(keymap-global-set "s-b" 'indent-buffer)
+(keymap-global-set "s-p" 'paredit-mode)
+(keymap-global-set "s-r" 'replace-regexp)
+(keymap-global-set "s-s" 'replace-string)
+(keymap-global-set "s-l" 'load-theme)
+(keymap-global-set "s-m" 'magit-status)
+(keymap-global-set "s-t" 'toggle-window-split)
+(keymap-global-set "s-v" 'magit-diff)
+(keymap-global-set "s-n" 'magit-blame)
+(keymap-global-set "s-/" 'comment-line-or-region)
+(keymap-global-set "s-\\" 'uncomment-line-or-region)
+(keymap-global-set "s-d" 'disable-themes)
+(keymap-global-set "s-w" 'whack-whitespace)
+(keymap-global-set "s-1" 'xterm-color-colorize-buffer)
+(keymap-global-set "s-3" 'kill-all-cider-buffers)
+
+
+;; (keymap-set clojure-mode-map (kbd "M-r") 'cider-namespace-refresh)
+
+(defadvice show-paren-function (after my-echo-paren-matching-line activate)
+  "If a matching paren is off-screen, echo the matching line."
+  (when (char-equal (char-syntax (char-before (point))) ?\))
+    (let ((matching-text (blink-matching-open) ))
+      (when matching-text
+        (message matching-text)))))
 
 (defadvice load-theme (before disable-before-load)
   "Disable any loaded themes before enabling a new theme.
@@ -265,8 +284,8 @@
 
 (ad-activate 'load-theme)
 
-(load-theme 'airline-cool)
-(load-theme 'atom-one-dark)
+;(load-theme 'airline-cool)
+;(load-theme 'atom-one-dark)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Inherit shell environment on OS X ;;
@@ -287,21 +306,21 @@
       (progn
         (save-buffers-kill-emacs))))
 
-(global-unset-key "\C-x\C-c")
-(global-set-key "\C-x\C-c" 'confirm-exit-emacs)
+(keymap-global-unset "C-x C-c")
+(keymap-global-set "C-x C-c" 'confirm-exit-emacs)
 
 
 ;; (load (expand-file-name "~/quicklisp/slime-helper.el"))
 ;; ;; Replace "sbcl" with the path to your implementation
 ;; (setq inferior-lisp-program "/usr/local/bin/sbcl")
 
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '(ns-appearance . dark))
+;; (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+;; (add-to-list 'default-frame-alist '(ns-appearance . dark))
 
 
 (unless window-system
-  (define-key input-decode-map "^[6~" [next])
-  (define-key input-decode-map "^[5~" [prior]))
+  (keymap-set input-decode-map "^[6~" [next])
+  (keymap-set input-decode-map "^[5~" [prior]))
 
 (defun helm-insert-char ()
   (interactive)
@@ -310,6 +329,17 @@
         (candidates . ,(ucs-names))
         (action . insert))))
 
+
+(defun toggle-transparency ()
+  (interactive)
+  (let ((alpha (frame-parameter nil 'alpha)))
+    (if (eq
+     (if (numberp alpha)
+         alpha
+       (cdr alpha)) ; may also be nil
+     100)
+    (set-frame-parameter nil 'alpha '(85 . 50))
+      (set-frame-parameter nil 'alpha '(100 . 100)))))
 
 ;;;;;;;;;;;;;;;;;;
 ;; Emacs Server ;;
@@ -320,5 +350,14 @@
 
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+
+(use-package ellama
+  :init
+  (setopt ellama-language "English")
+  (require 'llm-ollama)
+  (setopt ellama-provider
+		  (make-llm-ollama
+		   :chat-model "zephyr:latest" :embedding-model "zephyr:latest")))
+
 
 (provide 'emacs-mine)
